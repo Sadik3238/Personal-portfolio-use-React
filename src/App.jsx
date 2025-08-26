5ab4a8cd542a4c7c76aa97442a866758363b95ef
Mihun-Codex <mithunislaam390@gmail.com>
2025-08-27 01:26:22 +0600

    added all file

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+import Navbar from "./Components/Navbar"
+import Hero from "./Components/Hero"
+import About from "./Components/About"
+import Service from "./Components/Service"
+import Projects from "./Components/Projects"
+import Contact from "./Components/Contact"
+import Footer from "./Components/Footer"
+
+function App() {
+
+
+  return (
+    <>
+      <Navbar></Navbar>
+      <Hero></Hero>
+      <About></About>
+      <Service></Service>
+      <Projects></Projects>
+      <Contact></Contact>
+      <Footer></Footer>
+
+    </>
+
+  )
+}
+
+export default App
