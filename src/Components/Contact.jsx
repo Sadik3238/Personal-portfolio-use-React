350b53d3436f315e7283845d874505e27d236e8d
Mihun-Codex <mithunislaam390@gmail.com>
2025-08-27 01:31:55 +0600

    add change item

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
                 <h2 className='text-4xl font-bold text-center mb-12'>About Me</h2>
                 <div className='flex flex-col md:flex-row items-center md:space-x-12'>
                     <div className='flex-1'>
-                        <h3 className='text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r
+                        <h3 className='text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r
                                 from-green-400 to-blue-500'>Let's Talk</h3>
                         <p>I'm open to discussing web development projects or partnership
                             opportunities.</p>
